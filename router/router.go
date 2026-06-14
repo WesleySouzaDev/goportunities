8644bbae9b8f483ae03714d601e422404bda0b1f
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-06-14 17:45:11 -0300

    feat: add router subpackage

---
+++ router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initializer() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listens on 0.0.0.0:8080 by default
+}
