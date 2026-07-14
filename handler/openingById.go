16a2dd6f66766f346b5bdc548632c1df00640470
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-07-14 09:26:38 -0300

    feat: add openingById handler logic

---
+++ handler/openingById.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"goportunities/schemas"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ShowByIdOpeningHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "string").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	// Find opening
+	if err := db.First(&opening, &id).Error; err != nil {
+		logger.Errorf("error finding opening %v", err.Error())
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening not found with id %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-by-id-opening", opening)
+}
