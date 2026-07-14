e0e11f28c640ed19c18b3d9b6cbf3e8d0d595bac
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-07-14 09:12:29 -0300

    feat: add listOpening handler logic

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -1,13 +1,21 @@
 package handler
 
 import (
+	"goportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello World!",
-	})
+
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		logger.Errorf("error finding openings %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
