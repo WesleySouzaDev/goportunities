35c9109df0e0549e71123f61854cf758a41be737
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-07-18 22:04:05 -0300

    fix: add docs configuration models

--- handler/response.go
+++ handler/response.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"goportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -24,4 +25,30 @@ func sendSuccess(ctx *gin.Context, op string, data any) {
 		"message": fmt.Sprintf("operation from handler %s successful", op),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ListOpeningsResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+type ShowOpeningByIdResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
