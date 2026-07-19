d6e4015a82163c829d8514a290575f0385f15379
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-07-18 21:22:44 -0300

    feat: add update request schema

--- handler/request.go
+++ handler/request.go
@@ -41,3 +41,24 @@ func (r *CreateOpeningRequest) Validate() error {
 
 	return nil
 }
+
+// UpdateOpening
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// if any field is provided, validation is trythy
+	if r.Role != "" || r.Company != "" || r.Location != "" ||
+		r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+
+	// if name of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
